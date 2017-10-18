639b76d061c0548abdd2719c0ec48a1bf8557101
Bianca Angotti <bangotti@ualberta.ca>
2017-10-18 14:38:37 -0600

    create habit, habit event and user classes

---
+++ MyApplication/app/src/main/java/com/example/baard/User.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2017. Team CMPUT301F17T02, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at University of Alberta.
+ */
+
+package com.example.baard;
+
+/**
+ * Created by biancaangotti on 2017-10-18.
+ */
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+    private String username;
+
+    public User(String name, String email, String password, String username) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
