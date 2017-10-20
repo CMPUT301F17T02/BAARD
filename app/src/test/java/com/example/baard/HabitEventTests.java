b3ce3e23ac4d03dd96170ac1e3715e1af85a2d18
AndrewMcKernan <amckerna@ualberta.ca>
2017-10-20 13:28:32 -0600

    added @Before method that runs before each test case in order to reduce code duplication

--- app/src/test/java/com/example/baard/HabitEventTests.java
+++ app/src/test/java/com/example/baard/HabitEventTests.java
@@ -5,6 +5,8 @@
 package com.example.baard;
 
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -17,17 +19,33 @@ import static org.junit.Assert.*;
  */
 
 public class HabitEventTests {
-    //do we need this??
+
+    //do we need this?? was in the lab test cases
     //public HabitEventTests{
         //super();
     //}
 
-    public void testConstructor(){
-        ArrayList<Integer> freq = new ArrayList<Integer>();
-        Date date1 = new Date();
-        Habit habit = new Habit("test","reason",date1,freq);
+    ArrayList<Integer> freq;
+    Date date1;
+    Habit habit;
+    HabitEvent habitEvent;
+
+    //this function is run at the start of every test
+    @Before
+    public void setUpTest(){
+        freq = new ArrayList<Integer>();
+        date1 = new Date();
+        habit = new Habit("test","reason",date1,freq);
         // constructor 1
-        HabitEvent habitEvent = new HabitEvent(habit, "testevent", date1);
+        habitEvent = new HabitEvent(habit, "testevent", date1);
+    }
+
+    public void testConstructor(){
+        //ArrayList<Integer> freq = new ArrayList<Integer>();
+        //Date date1 = new Date();
+        //Habit habit = new Habit("test","reason",date1,freq);
+        // constructor 1
+        habitEvent = new HabitEvent(habit, "testevent", date1);
         // test that the values have been set
         assertEquals(habitEvent.getComment(), "testevent");
         assertEquals(habitEvent.getEventDate(), date1);
@@ -42,39 +60,39 @@ public class HabitEventTests {
     }
 
     public void testGetComment(){
-        ArrayList<Integer> freq = new ArrayList<Integer>();
-        Date date1 = new Date();
-        Habit habit = new Habit("test","reason",date1,freq);
+        //ArrayList<Integer> freq = new ArrayList<Integer>();
+        //Date date1 = new Date();
+        //Habit habit = new Habit("test","reason",date1,freq);
         // constructor 1
         HabitEvent habitEvent = new HabitEvent(habit, "testevent", date1);
         assertEquals(habitEvent.getComment(), "testevent");
     }
 
     public void testSetComment(){
-        ArrayList<Integer> freq = new ArrayList<Integer>();
-        Date date1 = new Date();
-        Habit habit = new Habit("test","reason",date1,freq);
+        //ArrayList<Integer> freq = new ArrayList<Integer>();
+        //Date date1 = new Date();
+        //Habit habit = new Habit("test","reason",date1,freq);
         // constructor 1
-        HabitEvent habitEvent = new HabitEvent(habit, "testevent", date1);
+        //HabitEvent habitEvent = new HabitEvent(habit, "testevent", date1);
         habitEvent.setComment("set test");
         assertEquals(habitEvent.getComment(), "set test");
     }
 
     public void testGetDate(){
-        ArrayList<Integer> freq = new ArrayList<Integer>();
-        Date date1 = new Date();
-        Habit habit = new Habit("test","reason",date1,freq);
+        //ArrayList<Integer> freq = new ArrayList<Integer>();
+        //Date date1 = new Date();
+        //Habit habit = new Habit("test","reason",date1,freq);
         // constructor 1
-        HabitEvent habitEvent = new HabitEvent(habit, "testevent", date1);
+        //HabitEvent habitEvent = new HabitEvent(habit, "testevent", date1);
         assertEquals(habitEvent.getEventDate(), date1);
     }
 
     public void testSetDate(){
-        ArrayList<Integer> freq = new ArrayList<Integer>();
-        Date date1 = new Date();
-        Habit habit = new Habit("test","reason",date1,freq);
+        //ArrayList<Integer> freq = new ArrayList<Integer>();
+        //Date date1 = new Date();
+        //Habit habit = new Habit("test","reason",date1,freq);
         // constructor 1
-        HabitEvent habitEvent = new HabitEvent(habit, "testevent", date1);
+        //HabitEvent habitEvent = new HabitEvent(habit, "testevent", date1);
         Date date2 = new Date();
         habitEvent.setEventDate(date2);
         assertNotEquals(habitEvent.getEventDate(), date1);
@@ -82,23 +100,23 @@ public class HabitEventTests {
     }
 
     public void testGetHabit(){
-        ArrayList<Integer> freq = new ArrayList<Integer>();
-        Date date1 = new Date();
-        Habit habit = new Habit("test","reason",date1,freq);
+        //ArrayList<Integer> freq = new ArrayList<Integer>();
+        //Date date1 = new Date();
+        //Habit habit = new Habit("test","reason",date1,freq);
         // constructor 1
-        HabitEvent habitEvent = new HabitEvent(habit, "testevent", date1);
+        //HabitEvent habitEvent = new HabitEvent(habit, "testevent", date1);
         assertEquals(habitEvent.getHabit(), habit);
     }
 
     public void testSetHabit(){
-        ArrayList<Integer> freq = new ArrayList<Integer>();
+        //ArrayList<Integer> freq = new ArrayList<Integer>();
         ArrayList<Integer> freq2 = new ArrayList<Integer>();
-        Date date1 = new Date();
+        //Date date1 = new Date();
         Date date2 = new Date();
-        Habit habit = new Habit("test","reason",date1,freq);
+        //Habit habit = new Habit("test","reason",date1,freq);
         Habit habit2 = new Habit("test2","better reason",date2,freq2);
         // constructor 1
-        HabitEvent habitEvent = new HabitEvent(habit, "testevent", date1);
+        //HabitEvent habitEvent = new HabitEvent(habit, "testevent", date1);
         habitEvent.setHabit(habit2);
         assertNotEquals(habitEvent.getHabit(), habit);
         assertEquals(habitEvent.getHabit(), habit2);
