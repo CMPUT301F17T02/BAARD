30cf9ad62795217f0e5e5d441b7f9cfedeaf35a3
AndrewMcKernan <amckerna@ualberta.ca>
2017-10-20 10:53:00 -0600

    beginnings of test cases for habitevent

---
+++ app/src/test/java/com/example/baard/HabitEventTests.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017. Team CMPUT301F17T02, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behaviour at University of Alberta.
+ */
+
+package com.example.baard;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by chrygore on 20/10/17.
+ */
+
+public class HabitEventTests {
+    //do we need this??
+    //public HabitEventTests{
+        //super();
+    //}
+
+    public void testCreateHabitEvent(){
+
+    }
+}
