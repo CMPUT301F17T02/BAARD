323fb0c593a745a0317ff216e16a4050623ea0a3
Bianca Angotti <bangotti@ualberta.ca>
2017-11-05 23:03:32 -0700

    fragment creation for main activity

--- app/src/main/java/com/example/baard/MainActivity.java
+++ app/src/main/java/com/example/baard/MainActivity.java
@@ -7,6 +7,7 @@ package com.example.baard;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,6 +17,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -39,7 +41,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -84,10 +86,24 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
+        if (id == R.id.nav_allHabits) {
+            Toast.makeText(this, "All Habits", Toast.LENGTH_SHORT).show();
+//            AllHabitsFragment allHabitsFragment = AllHabitsFragment.newInstance(0);
+//            FragmentManager manager = getSupportFragmentManager();
+//            manager.beginTransaction().replace(
+//                    R.id.relativelayout_for_fragment,
+//                    allHabitsFragment,
+//                    allHabitsFragment.getTag()
+//            ).commit();
+        } else if (id == R.id.nav_newHabit) {
+            Toast.makeText(this, "Create New Habit", Toast.LENGTH_SHORT).show();
+//            CreateNewHabitFragment createNewHabitFragment = CreateNewHabitFragment.newInstance("test", "test2");
+//            FragmentManager manager = getSupportFragmentManager();
+//            manager.beginTransaction().replace(
+//                    R.id.relativelayout_for_fragment,
+//                    createNewHabitFragment,
+//                    createNewHabitFragment.getTag()
+//            ).commit();
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
