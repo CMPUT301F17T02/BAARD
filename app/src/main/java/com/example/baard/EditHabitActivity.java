04754a9905afe56a76d9cf26e55dfd32eb5a3dc8
Bianca Angotti <bangotti@ualberta.ca>
2017-11-09 23:59:05 -0700

    edit from view

--- app/src/main/java/com/example/baard/EditHabitActivity.java
+++ app/src/main/java/com/example/baard/EditHabitActivity.java
@@ -4,28 +4,20 @@
 
 package com.example.baard;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class ViewHabitActivity extends AppCompatActivity {
+public class EditHabitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_habit);
+        setContentView(R.layout.activity_edit_habit);
     }
 
-    /* Called when the user taps the Edit button */
-    public void editHabit(View view) {
-        Intent intent = new Intent(this, EditHabitActivity.class);
-        startActivity(intent);
-    }
-
-    /* Called when the user taps the Delete button */
-    public void deleteHabit(View view) {
-        // TODO delete functionality
+    /* Called when the user taps the Save button */
+    public void saveHabit(View view) {
         finish();
     }
 }
