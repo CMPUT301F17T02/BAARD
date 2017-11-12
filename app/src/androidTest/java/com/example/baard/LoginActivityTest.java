58315785441a3a888124c08b66c6ce44e11d3c56
anarten <anarten@ualberta.ca>
2017-11-12 13:01:12 -0700

    Added JavaDoc to LoginActivityTest

--- app/src/androidTest/java/com/example/baard/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/baard/LoginActivityTest.java
@@ -13,26 +13,39 @@ import com.robotium.solo.Solo;
 
 /**
  * Created by Adam on 11/11/2017.
+ * Implements testing for the Login Activity
  */
-
 public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
     private Solo solo;
 
+    /**
+     * Constructor for the LoginActivityTest Class
+     */
     public LoginActivityTest() {
         super(com.example.baard.LoginActivity.class);
 
     }
 
+    /**
+     * Setup function for InstrumentationTest Cases
+     * @throws Exception
+     */
     public void setUp() throws Exception {
-        Log.d("SETUP", "First");
         solo = new Solo(getInstrumentation(), getActivity());
         Log.d("SETUP", "setUp()");
     }
 
+    /**
+     * Test Case to test starting the activity
+     * @throws Exception
+     */
     public void testStart() throws Exception {
         Activity activity = getActivity();
     }
 
+    /**
+     * Test case to test signing in a user and all associated errors
+     */
     public void testSignIn() {
         solo.assertCurrentActivity("wrong activity", LoginActivity.class);
         solo.clickOnButton("Sign in");
@@ -52,6 +65,9 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
         solo.clearEditText((EditText) solo.getView(R.id.username));
     }
 
+    /**
+     * Test case for registering a user and all associated errors
+     */
     public void testRegister() {
         solo.assertCurrentActivity("wrong activity", LoginActivity.class);
         solo.clickOnButton("Register");
@@ -77,9 +93,12 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
         solo.clearEditText((EditText) solo.getView(R.id.name));
     }
 
+    /**
+     * tearDown Function for InstrumentationTest Cases
+     * @throws Exception
+     */
     @Override
     public void tearDown() throws Exception {
         solo.finishOpenedActivities();
     }
-
 }
