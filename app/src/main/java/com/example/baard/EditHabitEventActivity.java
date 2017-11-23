b58645fc239a26964a6cc8572c96714007ebf8aa
Bianca Angotti <bangotti@ualberta.ca>
2017-11-22 21:03:17 -0700

    date parameters set

--- app/src/main/java/com/example/baard/EditHabitEventActivity.java
+++ app/src/main/java/com/example/baard/EditHabitEventActivity.java
@@ -101,6 +101,7 @@ public class EditHabitEventActivity extends AppCompatActivity {
                 DatePickerDialog d = new DatePickerDialog(EditHabitEventActivity.this, listener, calendar.get(Calendar.YEAR)
                         , calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
                 d.getDatePicker().setMaxDate((new Date()).getTime());
+                d.getDatePicker().setMinDate(habit.getStartDate().getTime());
                 d.show();
             }
         });
