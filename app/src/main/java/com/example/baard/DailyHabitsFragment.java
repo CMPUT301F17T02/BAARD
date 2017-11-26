2dbd4ecc97f74bfb49c897beea9af09231df52de
anarten <anarten@ualberta.ca>
2017-11-25 20:11:36 -0700

    Fixed import in Daily

--- app/src/main/java/com/example/baard/DailyHabitsFragment.java
+++ app/src/main/java/com/example/baard/DailyHabitsFragment.java
@@ -13,7 +13,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
+import android.widget.ExpandableListView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
