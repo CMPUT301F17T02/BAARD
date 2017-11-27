a9e8ef8d7755fd54a7a034b5090fea58561aeab5
rderbysh <rderbysh@ualberta.ca>
2017-11-27 16:08:27 -0700

    Added blank lists for now

--- app/src/main/java/com/example/baard/FindFriendsFragment.java
+++ app/src/main/java/com/example/baard/FindFriendsFragment.java
@@ -8,6 +8,8 @@ package com.example.baard;
  * Created by randi on 23/11/17.
  */
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -16,10 +18,14 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
 
 public class FindFriendsFragment extends Fragment {
 
-    private ListView habitListView;
+    private ListView findFriendsView;
     private ArrayAdapter<Habit> adapter;
     private HabitList habitList;
     private String username;
@@ -30,6 +36,21 @@ public class FindFriendsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_find_friends, container, false);
+
+        //fc = new FileController();
+
+        //SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+        //Gson gson = new Gson();
+        //String json = sharedPrefs.getString("username", "");
+        //username = gson.fromJson(json, new TypeToken<String>() {}.getType());
+
+        // dummy data for now just to play around with things
+        ArrayList<User> allUserList = new ArrayList<User>();
+        for (int i = 0; i < 5; i++) {
+            allUserList.add(new User(Integer.toString(i), Integer.toString(i), Integer.toString(i)));
+        }
         return rootView;
     }
+
+
 }
