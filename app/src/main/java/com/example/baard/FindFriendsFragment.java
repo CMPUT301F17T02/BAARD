e6bce66aba085b193388028e77db32498ccf314b
rderbysh <rderbysh@ualberta.ca>
2017-11-28 16:48:50 -0700

    Got text printing in list and buttons changing when clicked on

--- app/src/main/java/com/example/baard/FindFriendsFragment.java
+++ app/src/main/java/com/example/baard/FindFriendsFragment.java
@@ -32,16 +32,21 @@ import java.util.List;
 public class FindFriendsFragment extends Fragment {
 
     private ListView findFriendsView;
-    private ArrayAdapter<Habit> adapter;
-    private HabitList habitList;
+    private MyFriendsListAdapter adapter;
     private String username;
     private User user;
     private FileController fc;
+    List<User> allUserList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_find_friends, container, false);
+
+        for (int i = 0; i < 10; i++) {
+            allUserList.add(new User(Integer.toString(i), Integer.toString(i), Integer.toString(i)));
+            System.out.println(allUserList.get(i).getName());
+        }
         findFriendsView = (ListView) rootView.findViewById(R.id.findFriendsView);
 
 
@@ -51,17 +56,33 @@ public class FindFriendsFragment extends Fragment {
         Gson gson = new Gson();
         String json = sharedPrefs.getString("username", "");
         username = gson.fromJson(json, new TypeToken<String>() {}.getType());
-        ArrayList<String> data = new ArrayList<>();
+        //List<String> data = new ArrayList<>();
 
         // dummy data for now just to play around with things
-        ArrayList<User> allUserList = new ArrayList<User>();
-        for (int i = 0; i < 5; i++) {
-            allUserList.add(new User(Integer.toString(i), Integer.toString(i), Integer.toString(i)));
-        }
 
-        findFriendsView.setAdapter(new MyFriendsListAdapter(this.getContext(), R.layout.friend_list_item, allUserList));
+
+
+        adapter = new MyFriendsListAdapter(this.getContext(), R.layout.friend_list_item, allUserList);
+        findFriendsView.setAdapter(adapter);
+
+        adapter.notifyDataSetChanged();
 
         return rootView;
+    }
+
+    /**
+     * Called when AllHabitsFragment activity is opened up and called again.
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //user = fc.loadUser(getActivity().getApplicationContext(), username);
+
+        adapter = new MyFriendsListAdapter(this.getContext(), R.layout.friend_list_item, allUserList);
+        findFriendsView.setAdapter(adapter);
+
+        adapter.notifyDataSetChanged();
     }
 
     private class MyFriendsListAdapter extends ArrayAdapter<User> {
@@ -73,26 +94,28 @@ public class FindFriendsFragment extends Fragment {
 
         @NonNull
         @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
             ViewHolder mainViewHolder = null;
             if (convertView == null) {
                 LayoutInflater inflater = LayoutInflater.from(getContext());
                 convertView = inflater.inflate(layout, parent, false);
-                ViewHolder viewHolder = new ViewHolder();
+                final ViewHolder viewHolder = new ViewHolder();
                 viewHolder.title = (TextView) convertView.findViewById(R.id.addFriend);
                 viewHolder.button = (Button) convertView.findViewById(R.id.addFriendButton);
                 viewHolder.button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        if (viewHolder.button.getText() == "UNFOLLOW") {
+                            viewHolder.button.setText("FOLLOW");
+                        } else { viewHolder.button.setText("UNFOLLOW"); }
 
+                        // set friend to list.
                     }
                 });
                 convertView.setTag(viewHolder);
             }
-            else {
-                mainViewHolder = (ViewHolder) convertView.getTag();
-                mainViewHolder.title.setText((CharSequence) getItem(position));
-            }
+            mainViewHolder = (ViewHolder) convertView.getTag();
+            mainViewHolder.title.setText(getItem(position).getName());
 
             return convertView;
         }
