406fdad36f86573007c94cd79d67b8ebb9d40097
rderbysh <rderbysh@ualberta.ca>
2017-12-03 22:35:10 -0700

    Pulled from master and fixed User Tests

--- app/src/main/java/com/example/baard/FriendRequestsFragment.java
+++ app/src/main/java/com/example/baard/FriendRequestsFragment.java
@@ -14,11 +14,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.Button;
 import android.widget.ExpandableListView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
